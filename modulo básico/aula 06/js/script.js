9689f9c016f84f3e502bf1887cf3ba7f9f1a495f
Joaov535 <joaovictorcruz535@gmail.com>
2022-10-24 21:06:20 -0300

    24/10

---
+++ modulo básico/aula 06/js/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Dígite o primeiro valor');
+let num2 = prompt('Dígite o segundo valor');
+num1 = Number(num1);
+num2 = Number(num2);
+const soma = num1 + num2 ;
+window.alert('A soma é ' + soma)
