3da7aae8831c3450ff00a7fe5318470cb98a91bf
Joaov535 <joaovictorcruz535@gmail.com>
2022-10-27 10:05:10 -0300

    modulo terminado

---
+++ modulo básico/exercicio/js/script.js
@@ -0,0 +1,29 @@
+function meuEscopo() {
+    const formulario = document.querySelector('.form');
+    const resultado = document.querySelector('.res');
+
+    const pessoas = [];
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+
+        const nome = formulario.querySelector('.nome');
+        const sobrenome = formulario.querySelector('.sobrenome');
+        const peso = formulario.querySelector('.peso');
+        const altura = formulario.querySelector('.altura');
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+    }
+
+    formulario.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
