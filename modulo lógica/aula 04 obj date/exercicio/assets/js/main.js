6fb117340c0c6013f3d77d299c53a16501115c97
Joaov535 <joaovictorcruz535@gmail.com>
2022-11-02 11:45:48 -0300

    ex switch e Date

---
+++ modulo lógica/aula 04 obj date/exercicio/assets/js/main.js
@@ -0,0 +1,93 @@
+function escopo() {
+
+    const data = new Date();
+    const dia = data.getDate();
+    const hora = data.getHours();
+    const minutos = data.getMinutes();
+    const ano = data.getFullYear();
+
+    function txtMes(mes) {
+        let textoMes;
+
+        switch (mes) {
+            case 0:
+                textoMes = 'Janeiro';
+                return textoMes;
+            case 1:
+                textoMes = 'Fevereiro';
+                return textoMes;
+            case 2:
+                textoMes = 'Março';
+                return textoMes;
+            case 3:
+                textoMes = 'Abril';
+                return textoMes;
+            case 4:
+                textoMes = 'Maio';
+                return textoMes;
+            case 5:
+                textoMes = 'Junho';
+                return textoMes;
+            case 6:
+                textoMes = 'Julho';
+                return textoMes;
+            case 7:
+                textoMes = 'Agosto';
+                return textoMes;
+            case 8:
+                textoMes = 'Setembro';
+                return textoMes;
+            case 9:
+                textoMes = 'Outubro';
+                return textoMes;
+            case 10:
+                textoMes = 'Novembro';
+                return textoMes;
+            case 11:
+                textoMes = 'Dezembro';
+                return textoMes;
+        };
+    };
+    
+
+    const mes = data.getMonth();
+    const mesTxt = txtMes(mes);
+    console.log(mesTxt);
+
+    function diaSemana(numDia) {
+        let diaSemTexto;
+
+        switch (numDia) {
+            case 0:
+                diaSemTexto = 'Domingo';
+                return diaSemTexto;
+            case 1:
+                diaSemTexto = 'Segunda-Feira';
+                return diaSemTexto;
+            case 2:
+                diaSemTexto = 'Terça-Feira';
+                return diaSemTexto;
+            case 3:
+                diaSemTexto = 'Quarta-Feira';
+                return diaSemTexto;
+            case 4:
+                diaSemTexto = 'Quinta-Feira';
+                return diaSemTexto;
+            case 5:
+                diaSemTexto = 'Sexta-Feira';
+                return diaSemTexto;
+            case 6:
+                diaSemTexto = 'Sabado';
+                return diaSemTexto;
+        };
+    };
+
+    const semana = data.getDay();
+    const diaSemTxt = diaSemana(semana);
+    console.log(diaSemTxt);
+
+    const paragrafo = document.querySelector('#infoDia')
+    paragrafo.innerHTML = `${diaSemTxt}, ${dia} de ${mesTxt} de ${ano} ${hora}:${minutos}`
+
+}
+
