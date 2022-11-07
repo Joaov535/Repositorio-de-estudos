42745f0cc5d064543728656a636b26093dbaa797
Joaov535 <joaovictorcruz535@gmail.com>
2022-11-07 10:31:39 -0300

    for of

---
+++ modulo lógica/aula 11 for of/index.js
@@ -0,0 +1,11 @@
+//For of itera sobre os valores do objeto
+const nomes = ['João', 'Arnaldo', 'Sophia'];
+
+for (const i of nomes) {
+    console.log(i);
+}
+
+//## Resumo dos for 
+//For -> Geralmente com iteráveis (arrays ou strings)
+//For in -> Retorna o índice ou chave (string, array ou objetos)
+//For of -> Retorna o valor em si (iteráveis, arrays ou strings)
