dc8805c553112c0bc6e71515fbbd175acd5d1861
Joaov535 <joaovictorcruz535@gmail.com>
2023-04-17 18:34:48 -0300

    manipulando arquivos externos

---
+++ PHP/PHP/manipulando-arquivos/excluindo/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <p>Para excluir um arquivo externo basta usar a função <strong>unlink()</strong></p>
+    <p>sintáxe: unlink($arquivo)</p>
+    <p>sintáxe: unlink('arquivo.txt')</p>
+</body>
+</html>
---
+++ PHP/PHP/manipulando-arquivos/renomeando-movendo/index.php
@@ -0,0 +1,15 @@
+<?php 
+# rename recebe 2 parâmtros
+# 1° o caminho do arquivo
+# 2° o novo nome do arquivo
+# rename('arquivo-um.txt', 'arquivo-dois.txt');
+rename('teste.txt', 'pasta/teste.txt');
+?>
+
+<p>O arquivo foi renomeado de arquivo-um.txt para arquivo-dois.txt</p>
+<p>utulizando a função rename()</p>
+<p>acesso o código donte para mais informações</p>
+<hr>
+<p>O arquivo teste.txt foi movido usando a função rename()</p>
+
+<?php ?>
